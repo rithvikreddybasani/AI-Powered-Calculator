1f8479bd9453e3bf739729d7cdd690246a7abf90
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-09-17 09:48:29 +0530

    Update main.py

--- calc-be-main/main.py
+++ calc-be-main/main.py
@@ -1,16 +1,22 @@
+import os
+from dotenv import load_dotenv
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from apps.calculator.route import router as calculator_router
-from constants import SERVER_URL, PORT, ENV
+
+load_dotenv()  # Load environment variables
+
+SERVER_URL = os.getenv("SERVER_URL", "0.0.0.0")
+PORT = os.getenv("PORT", "8000")  # Default to 8000 if not set
+ENV = os.getenv("ENV", "dev")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,13 +26,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get('/')
 async def root():
     return {"message": "Server is running"}
 
 app.include_router(calculator_router, prefix="/calculate", tags=["calculate"])
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=SERVER_URL, port=int(PORT), reload=(ENV == "dev"))

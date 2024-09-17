8ebdd2c5300810aae8dcafb3dcf912b9669b265a
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-09-17 10:24:41 +0530

    Update vite.config.ts

--- calc-front-main/vite.config.ts
+++ calc-front-main/vite.config.ts
@@ -1,13 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
+import path from "path";
+import react from "@vitejs/plugin-react";
 import eslint from 'vite-plugin-eslint';
-import { defineConfig } from "vite"
- 
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react(),eslint()],
+  plugins: [react(), eslint()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    host: '0.0.0.0',  
+  },
+});
